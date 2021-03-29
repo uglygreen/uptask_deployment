6408260ede79ee08f478c064c85badb10825bd21
Jesus Lopez <sonnama.32@gmail.com>
2021-03-28 23:32:57 -0600

    solucionado el mensaje del servidor listo

--- index.js
+++ index.js
@@ -75,5 +75,5 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.listen(port, host, () =>{
-    console.log('El servidor esta funcionando')
+    console.log('El servidor esta Listo!')
 });
